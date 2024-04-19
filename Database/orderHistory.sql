121cda0862fc0c1577f5a2990ef981ecbb25aec3
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-04-19 16:20:20 +0530

    SQL: Initialize orderHistory schema

---
+++ Database/orderHistory.sql
@@ -0,0 +1,20 @@
+CREATE table orderHistory(
+    buyerId NUMBER,
+    itemNO NUMBER,
+    sellerID NUMBER,
+    orderDate DATE CONSTRAINT orderDate_not_null not null,
+    status VARCHAR2(10) CONSTRAINT status_not_null not null,
+
+    CONSTRAINT fk_seller FOREIGN KEY (sellerID) REFERENCES userTable(userID),
+    CONSTRAINT fk_seller FOREIGN KEY (buyerID) REFERENCES userTable(userID),
+    CONSTRAINT fk_seller FOREIGN KEY (itemNO) REFERENCES item(itemNO)
+);
+drop table orderHistory;
+
+INSERT ALL
+INTO orderHistory(buyerId,itemNO,sellerID,orderDate,status)
+VALUES (219,7,203,TO_DATE('2/3/24','mm/dd/yy'),'Done')
+SELECT * FROM dual;
+
+SELECT * from TAB;
+SELECT * from USERTABLE;

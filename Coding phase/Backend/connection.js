9b97d36cea3812422aaed169df946e95878365c8
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2024-09-14 01:59:10 +0530

    Refactor db connection to use environment variables and connect mysql server with routes

--- Coding phase/Backend/connection.js
+++ Coding phase/Backend/connection.js
@@ -1,17 +1,19 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-
+const railwayDB=`mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}`
 
 let db;
 
 function handleDisconnect() {
-    db = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    });
+    // db = mysql.createConnection({
+    //     host: process.env.DB_HOST,
+    //     user: process.env.DB_USER,
+    //     password: process.env.DB_PASSWORD,
+    //     database: process.env.DB_NAME
+    // });
+
+    db = mysql.createConnection(railwayDB); //connect to railway database
 
     db.connect(err => {
         if (err) {

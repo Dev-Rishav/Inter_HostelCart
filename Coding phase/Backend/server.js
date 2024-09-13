61bf9f59348aaedbe1c1a5cb4f370a9d1eb7171a
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2024-09-14 01:23:07 +0530

    Connect mysql server with routes

--- Coding phase/Backend/server.js
+++ Coding phase/Backend/server.js
@@ -1,14 +1,49 @@
-const express= require('express');
-const  mysql = require('mysql');
-const cors= require('cors');
-
+const mysql = require('mysql');
+const express = require('express');
 const app = express();
-app.use(cors());
+
+let db;
+
+function handleDisconnect() {
+    db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Str0ngP@ssw0rd!',
+        database: 'InterHostelCart'
+    });
+
+    db.connect(err => {
+        if (err) {
+            console.error('Error connecting to database:', err);
+            setTimeout(handleDisconnect, 2000); // Reconnect after 2 seconds
+        }
+    });
+
+    db.on('error', err => {
+        console.error('Database error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect(); // Reconnect if connection is lost
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
 
 app.get('/', (req, res) => {
     res.send("From backend");
-}
-)
+});
+
+app.get('/products', (req, res) => {
+    const sqlSelect = "SELECT * FROM products";
+    db.query(sqlSelect, (err, result) => {
+        if (err) {
+            return res.json({ error: err });
+        }
+        res.send(result);
+    });
+});
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");

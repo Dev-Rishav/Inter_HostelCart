10fa9d2bac6b8d23fb641426f4550045fc6d7a74
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2024-09-18 18:43:18 +0530

    Refactor file paths and component names in App.jsx

--- Coding phase/Frontend/src/App.jsx
+++ Coding phase/Frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
 import React from 'react';
-import Home from "./pages/home";
+import Home from "./pages/Home";
+import Mens from "./pages/Mens";
+import MyShop from "./pages/MyShop";
+import Womens from "./pages/Womens";
+import Contact from "./pages/Contact";
+
+
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>    
+      <Route path="/" element={<Home />} />
+        <Route path="/myShop" element={<MyShop />} />
+        <Route path="/womens" element={<Womens />} />
+        <Route path="/mens" element={<Mens />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   )
---
+++ Coding phase/Frontend/src/pages/Mens.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const mens = () => {
+  return (
+    <div>mens</div>
+  )
+}
+
+export default mens

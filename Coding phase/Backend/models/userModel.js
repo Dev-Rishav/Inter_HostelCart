2226c8a57cc78f3ef5bd52f9ed955ad0c75c4b49
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2024-11-06 13:20:40 +0530

    Refactor userModel for user registration

--- Coding phase/Backend/models/userModel.js
+++ Coding phase/Backend/models/userModel.js
@@ -1,9 +1,9 @@
 const pool = require('../config/db');
 
 const User = {
-  create: (username, hash, email, callback) => {
-    const sqlInsert = "INSERT INTO usertable (username, userpassword, emailid) VALUES ($1, $2, $3)";
-    pool.query(sqlInsert, [username, hash, email], callback);
+  create: (email,hostel,room,username,dob,phone,hash,dept,course, callback) => {
+    const sqlInsert = "INSERT INTO usertable (emailid,hostelno,roomno,username,userdob,userphoneno,userpassword,userdept,usercourse) VALUES ($1, $2, $3,$4,$5,$6,$7,$8,$9)";
+    pool.query(sqlInsert, [email,hostel,room,username,dob,phone,hash,dept,course], callback);
   },
   findByEmail: (email, callback) => {
     const sqlSelect = "SELECT * FROM usertable WHERE emailid = $1";

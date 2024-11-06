04b52097703c29abae1c71c91b1154f7edccce5c
Rishav Chakraborty <53538405+Dev-Rishav@users.noreply.github.com>
2024-11-06 12:57:37 +0530

    Finialize user authentication

--- Coding phase/Backend/models/userModel.js
+++ Coding phase/Backend/models/userModel.js
@@ -2,11 +2,11 @@ const pool = require('../config/db');
 
 const User = {
   create: (username, hash, email, callback) => {
-    const sqlInsert = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+    const sqlInsert = "INSERT INTO usertable (username, userpassword, emailid) VALUES ($1, $2, $3)";
     pool.query(sqlInsert, [username, hash, email], callback);
   },
   findByEmail: (email, callback) => {
-    const sqlSelect = "SELECT * FROM userTable WHERE emailID = ?";
+    const sqlSelect = "SELECT * FROM usertable WHERE emailid = $1";
     pool.query(sqlSelect, [email], callback);
   }
 };
